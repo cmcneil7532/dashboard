610ab37f9f04af9142e0aee384a7a9331765ae5d
Christian McNeil <christianmcneil@ChristiansMBP2.fios-router.home>
2022-12-06 19:51:57 -0500

    Creation of kanban still using syncfusion

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -3,6 +3,7 @@ import {
   KanbanComponent,
   ColumnDirective,
   ColumnsDirective,
+  Inject,
 } from "@syncfusion/ej2-react-kanban";
 
 import { kanbanData, kanbanGrid } from "../data/dummy";
@@ -10,8 +11,23 @@ import { kanbanData, kanbanGrid } from "../data/dummy";
 import { Header } from "../components";
 const Kanban = () => {
   return (
-    <div>
-      <Header category="App" title="Kanba" />
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Kanban" />
+      <KanbanComponent
+        id="kanban"
+        //Fetch the data to put inside the grid
+        dataSource={kanbanData}
+        cardSettings={{ contentField: "Summary", headerField: "id" }}
+        keyField="Status"
+      >
+        {/** Loop over the columns directive */}
+        <ColumnsDirective>
+          {/** Map out the  */}
+          {kanbanGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
     </div>
   );
 };
