1ccfb715d349e8ea4e566a058e122f864985a633
Christian McNeil <christianmcneil@ChristiansMBP2.fios-router.home>
2022-12-06 19:28:29 -0500

    Creation of the calendar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+import { scheduleData } from "../data/dummy";
 
+import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
+import { Header } from "../components";
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    //On regular devices margin of 2 and paddin of 2 on medium devices margin of 10 and padding of 10
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent
+        height="560px"
+        //Passing in our data dummy into our schedule component
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2021, 0, 10)}
+      >
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, DragAndDrop, Resize]}
+        />
+      </ScheduleComponent>
+    </div>
+  );
 };
 
 export default Calendar;
--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import {
+  KanbanComponent,
+  ColumnDirective,
+  ColumnsDirective,
+} from "@syncfusion/ej2-react-kanban";
 
+import { kanbanData, kanbanGrid } from "../data/dummy";
+
+import { Header } from "../components";
 const Kanban = () => {
-  return <div>Kan</div>;
+  return (
+    <div>
+      <Header category="App" title="Kanba" />
+    </div>
+  );
 };
 
 export default Kanban;
