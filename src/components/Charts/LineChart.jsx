be8e6bda02f1179469281d5e97336a26f98b0e95
Christian McNeil <christianmcneil@ChristiansMBP2.fios-router.home>
2022-12-08 21:30:37 -0500

    Finalized the area page

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  Tooltip,
+  LineSeries,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  lineCustomSeries,
+  LinePrimaryYAxis,
+  LinePrimaryXAxis,
+} from "../../data/dummy";
+
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const LineChart = () => {
-  return <div>LineChart</div>;
+  const { currentMode } = useStateContext();
+  return (
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      //Makes the x axis values more precise
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      //Hover over each point to get mpre precise deatils
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#333735" : "#fff"}
+      chartArea={{ border: { width: 0 } }}
+    >
+      <Inject services={[DateTime, Legend, Tooltip, LineSeries]} />
+
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
 };
 
 export default LineChart;
--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  SplineAreaSeries,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  areaCustomSeries,
+  areaPrimaryYAxis,
+  areaPrimaryXAxis,
+} from "../../data/dummy";
+
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Header } from "../../components";
 
 const Area = () => {
-  return <div>Area</div>;
+  const { currentMode } = useStateContext();
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg   rounded-3xl">
+      <Header category="Chart" title="Inflation Rate in Percentage" />
+      <ChartComponent
+        id="area-chart"
+        height="420px"
+        //Makes the x axis values more precise
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        //Hover over each point to get mpre precise deatils
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#333735" : "#fff"}
+        chartArea={{ border: { width: 0 } }}
+      >
+        <Inject services={[DateTime, Legend, SplineAreaSeries]} />
+
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export default Area;
